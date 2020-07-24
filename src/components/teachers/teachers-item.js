93f6e7d90a11d13c6ed361a8210a0a9f5e1a52ce
EdwinTinoco <jaredlotic@gmail.com>
2020-07-24 15:13:56 -0600

    added media queries students items

--- src/components/teachers/teachers-item.js
+++ src/components/teachers/teachers-item.js
@@ -10,13 +10,11 @@ export default class GradeAndGroupsItem extends Component {
    render() {
       return (
          <div className="grades-groups-item-wrapper">
-
             <div className='grades-groups-links-wrapper'>
                <Link to={`/students-home/${this.props.item.grades_groups_id}`}>
                   {this.props.item.grades_groups_name}
                </Link>
             </div>
-
          </div>
       )
    }
