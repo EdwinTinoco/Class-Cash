01d83ae55d3bdff1becfef6136223f625b6aa790
EdwinTinoco <jaredlotic@gmail.com>
2020-08-26 08:34:45 -0600

    cleaned code

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -2,22 +2,16 @@ import {
    faTrash,
    faSignOutAlt,
    faEdit,
-   faSpinner,
    faPlusCircle,
    faMinusCircle,
-   faPhone,
-   faEnvelope,
-   faMapMarkedAlt,
    faLock,
    faLink,
-   faMapMarkerAlt,
-   faCalculator,
-   faCamera,
    faBars,
    faTimes,
    faUser,
    faFileExcel
 } from "@fortawesome/free-solid-svg-icons";
+
 import { library } from "@fortawesome/fontawesome-svg-core";
 
 const Icons = () => {
@@ -25,17 +19,10 @@ const Icons = () => {
       faTrash,
       faSignOutAlt,
       faEdit,
-      faSpinner,
       faPlusCircle,
       faMinusCircle,
-      faPhone,
-      faEnvelope,
-      faMapMarkedAlt,
       faLock,
       faLink,
-      faMapMarkerAlt,
-      faCalculator,
-      faCamera,
       faBars,
       faTimes,
       faUser,
